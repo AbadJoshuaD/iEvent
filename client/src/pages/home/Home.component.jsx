442a79472639862de3c425d302ccd9a3a04d38ed
Abad Joshua <abad.joshuad@gmail.com>
2019-11-08 22:53:21 +0800

    testing branching

--- client/src/pages/home/Home.component.jsx
+++ client/src/pages/home/Home.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../home/Home.styles.scss";
 
-const HomePage = () => <div>Hello Page</div>;
+const HomePage = () => <div>Hello Page World</div>;
 
 export default HomePage;
